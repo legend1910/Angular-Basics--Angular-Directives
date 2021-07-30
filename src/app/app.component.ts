10a7586497280baf7de26d7a99d577e7888f385d
legend1910 <legend.yuva@gmail.com>
2021-07-31 02:40:07 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = '';
+  isNameAdded = false;
+  OnAddEvent() {
+    this.isNameAdded = true;
+    console.log(this.name);
+  }
 }
